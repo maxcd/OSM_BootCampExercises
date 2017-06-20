101fbf2ad8085cbba8edc86957697d60b1b112e6
Max Diegel <max.diegel@posteo.de>
2017-06-20 15:45:18 -0500

    updated solutions for StandardLibrary

---
+++ Computation/Wk1_pyIntro/calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 15:30:56 2017
+
+@author: Max
+"""
+def sum2(a, b):
+    return a + b
+
+def prod2(a, b):
+    return a * b
+
+def sq_root(a):
+    return a ** 0.5
+    
+    
